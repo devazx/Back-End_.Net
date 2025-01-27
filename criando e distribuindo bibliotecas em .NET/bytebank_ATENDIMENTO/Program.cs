1ee50b90e9a51dcebed5e544f8ea2469019fe1a7
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-01-27 20:55:28 -0300

    Autenticador com arquivos separados e exporte de arquivo json

--- criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/Program.cs
+++ criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/Program.cs
@@ -1,5 +1,7 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 new ByteBankAtendimento().AtendimentoCliente();
 
+//AutenticacaoUtil autenticador = new AutenticacaoUtil(); protegido com internal
 
