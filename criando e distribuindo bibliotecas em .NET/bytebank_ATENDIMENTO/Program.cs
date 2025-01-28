cf69a6df04d87cf74ee0238b9b5fd7df262908e7
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-01-27 21:41:33 -0300

    ADC referencia COM outro arquivo separado

--- criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/Program.cs
+++ criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/Program.cs
@@ -1,24 +1,27 @@
 ﻿using bytebank.Modelos.ADM.Funcionarios;
 using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_GeradorChavePix;
 using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
+
+Console.WriteLine(GeradorPix.GetChavePix());
 
 //AutenticacaoUtil autenticador = new AutenticacaoUtil(); protegido com internal
 
-public class Estagiario : Funcionario
-{
-    public Estagiario(double salario, string cpf) : base(salario, cpf)
-    {
-    }
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+//    }
 
-    public override void AumentarSalario()
-    {
-        throw new NotImplementedException();
-    }
+//    public override void AumentarSalario()
+//    {
+//        throw new NotImplementedException();
+//    }
 
-    protected override double getBonificacao()
-    {
-        throw new NotImplementedException();
-    }
-}
+//    protected override double getBonificacao()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
