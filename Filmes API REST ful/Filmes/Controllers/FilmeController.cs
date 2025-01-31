78aa8d8f81a1f0f04c031a0965d61f7a2b86b198
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-01-31 20:37:37 -0300

    API rest ful em ASP.NET

---
+++ Filmes API REST ful/Filmes/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using Filmes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+
+        }
+    }
+}
