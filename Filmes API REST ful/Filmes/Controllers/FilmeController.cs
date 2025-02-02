0c6e5e98407d3e6d3de44a2a0171803eecb21115
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-02-02 15:32:49 -0300

    sistema de busca por ID e geral, HTTP post e GET

--- Filmes API REST ful/Filmes/Controllers/FilmeController.cs
+++ Filmes API REST ful/Filmes/Controllers/FilmeController.cs
@@ -9,14 +9,28 @@ namespace Filmes.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
 
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+
+        }
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmePorID(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id.Equals(id));
         }
     }
 }
--- Filmes API REST ful/Filmes/Models/Filme.cs
+++ Filmes API REST ful/Filmes/Models/Filme.cs
@@ -1,9 +1,17 @@
-﻿namespace Filmes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filmes.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O genero do filme é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres")]
         public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duracao tem que ser entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
