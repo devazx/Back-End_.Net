8aad8a238b3de71ed2ca62739fc7a43903b64826
Daniel Vaz <danielvaz.tzk@hotmail.com>
2025-02-24 21:13:59 -0300

    Adc genero ao contexto de Cadastro

--- ApiUsuarios/ApiUsuarios/Models/Usuario.cs
+++ ApiUsuarios/ApiUsuarios/Models/Usuario.cs
@@ -5,6 +5,7 @@ namespace ApiUsuarios.Models
     public class Usuario : IdentityUser
     {
         public DateTime DataNascimento { get; set; }
+        public string Genero { get; set; }
         public Usuario(): base() { }
     }
 }
